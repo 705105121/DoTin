f86a649e2579acd49a9c4cf36aec1db12d784033
Tin <dotin1611@gmail.com>
2023-03-01 00:09:00 +0700

    Phien ban thu 3

--- code.h
+++ code.h
@@ -1,5 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void sap xep day so(int *a,int n);
-int tim_max (int *a, int n);
+void sap xep day so(int* a,int n);
+int tim_max (int* a, int n);
+int tim_min (int* a, int n);
+void print_value(int* a, int n);
